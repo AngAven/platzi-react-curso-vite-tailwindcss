788b596d05108927a908785f043a1f1e210f01fa
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-02-19 02:46:55 -0600

    Start Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+      <>
+          <h1 className="text-3xl font-bold underline">
+              Hello world!
+          </h1>
+
+          <div className='bg-blue-500'>
+              <p>Hello</p>
+          </div>
+      </>
+  )
+}
+
+export default App
