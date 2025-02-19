4900b382d2692efe26b5eac9df058909a4529f52
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-02-19 03:07:22 -0600

    Deploy gh-pages, configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: 'https://angaven.github.io/platzi-react-curso-vite-tailwindcss/',
   plugins: [
       react(),
       tailwindcss(),
